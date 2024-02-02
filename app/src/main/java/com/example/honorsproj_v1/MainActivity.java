e64ab3fc4b6bf24867e210a60ba5cded14f72bd7
dengli300 <dengli300@caledonian.ac.uk>
2024-02-02 22:56:36 +0000

    Test. First git commit with those two dropdown menus

---
+++ app/src/main/java/com/example/honorsproj_v1/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.honorsproj_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String[] item = {"Material", "Design", "Components", "Android", "Lollipop"};
+
+    AutoCompleteTextView autoCompleteTextView1;
+    AutoCompleteTextView autoCompleteTextView2;
+
+    ArrayAdapter<String> adapterItems;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autoCompleteTextView1 = findViewById(R.id.auto_complete_textview1);
+        autoCompleteTextView2 = findViewById(R.id.auto_complete_textview2);
+
+        adapterItems = new ArrayAdapter<>(this,R.layout.list_item, item);
+
+        autoCompleteTextView1.setAdapter(adapterItems);
+        autoCompleteTextView2.setAdapter(adapterItems);
+
+        autoCompleteTextView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String item = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, "item   " + item, Toast.LENGTH_SHORT).show();
+            }
+        });
+        autoCompleteTextView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String item = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, "item   " + item, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
